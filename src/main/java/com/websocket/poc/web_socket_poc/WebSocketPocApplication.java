0d582aa9e50e16b5e6141de7666814d9f89d823a
Vivek-ag1010 <vivek.agrahari@xcelore.com>
2024-08-07 14:40:57 +0530

    Initial Commit

---
+++ src/main/java/com/websocket/poc/web_socket_poc/WebSocketPocApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.poc.web_socket_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketPocApplication.class, args);
+	}
+
+}
